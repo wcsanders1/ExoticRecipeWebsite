5bde4e5e7641924ea3651e3b76f4ac012c819716
William Sanders <william.sanders.dev@gmail.com>
2016-08-17 18:08:29 -0400

    added jquery to home page image

---
+++ ExoticRecipeWebsite/Scripts/Site.js
@@ -0,0 +1,25 @@
+﻿var $overlay = $("<div id='overlay'></div>");
+var $caption = $("<p></p>");
+var $dish_image = $("<img>");
+$overlay.append($dish_image);
+$overlay.append($caption);
+$(".container-body-content").prepend($overlay);
+$overlay.hide();
+
+$(".recipe-of-day-figure img").click(function () {
+    if ($("#overlay").css("z-index") == 10) {
+        var imageLocation = $(this).attr("src");
+        $dish_image.attr("src", imageLocation);
+        $overlay.slideDown();
+        var captionText = $(".title").text();
+        $caption.text(captionText);
+    }
+});
+
+$overlay.click(function () {
+    $(this).hide();
+})
+
+$(window).resize(function () {
+    $("#overlay").hide();
+});
