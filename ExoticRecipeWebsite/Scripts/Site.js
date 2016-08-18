a7ee9de525bc01b1d106f2c16ae746006aab81dd
William Sanders <william.sanders.dev@gmail.com>
2016-08-18 19:09:55 -0400

    added tooltip to recipe image

--- ExoticRecipeWebsite/Scripts/Site.js
+++ ExoticRecipeWebsite/Scripts/Site.js
@@ -1,4 +1,6 @@
-﻿var $overlay = $("<div id='overlay'></div>");
+﻿//******** **********OVERLAY FOR HOMEPAGE ******************************
+
+var $overlay = $("<div id='overlay'></div>");
 var $caption = $("<p></p>");
 var $dish_image = $("<img>");
 $overlay.append($dish_image);
@@ -22,4 +24,23 @@ $overlay.click(function () {
 
 $(window).resize(function () {
     $("#overlay").hide();
+});
+
+
+//******************* TOOLTIP FOR RECIPE PICTURE ****************
+
+var $tip = $("<p class='tooltip'>Click for larger view!</p>");
+
+$(".recipe-of-day-figure img").hover(function () {
+    if ($("#overlay").css("z-index") == 10)
+    {
+        $(".recipe-of-day-figure").append($tip);
+        $tip.fadeIn("slow");
+    }
+}, function () {
+    $(".tooltip").remove();
+}).mousemove(function (e) {
+    var mousex = e.pageX - 50;
+    var mousey = e.pageY - 390;
+    $(".tooltip").css({ top: mousey, left: mousex })
 });
