da4e99327bcf80f40a719761f4e470022a9ae65f
William Sanders <william.sanders.dev@gmail.com>
2016-09-17 22:57:27 -0400

    cross-browser compatibility

---
+++ ExoticRecipeWebsite/Scripts/contact.js
@@ -0,0 +1,23 @@
+﻿function SizeForIE() {
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    console.log(msie);
+
+    if (/MSIE 10/i.test(navigator.userAgent)) {   //doesn't work
+
+        //position container body content
+
+        $containerBodyContent = $("#container-body-content");
+
+        var spaceBetweenHeaderAndFooter = $("footer").position().top - ($("header").position().top + $("header").height());
+
+        console.log(spaceBetweenHeaderAndFooter);
+        $containerBodyContent.css("margin-top", (spaceBetweenHeaderAndFooter / 2));
+    }
+}
+
+$(document).ready(function () {
+    SizeForIE();
+});
